1872bfc093cb7267300509be4b34ba07e09dede6
unknown <yvonsplanter@gmail.com>
2019-04-28 13:43:20 -0400

    Add model

---
+++ TP1/Model/CatalogPhones.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TP1.Model
+{
+    public class CatalogPhone
+    {
+        public List<Phone> Phones { get; set; }
+
+        public CatalogPhone() {
+            this.Phones = new List<Phone>();
+        }
+    }
+}
---
+++ TP1/Model/Phone.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TP1.Model
+{
+    public class Phone
+    {
+        public string ID { get; set; }
+        public string Name { get; set; }
+        public float Price { get; set; }
+        public string Image { get; set; }
+        public string Description { get; set; }
+
+        public Phone() {
+            this.ID="";
+            this.Name = "";
+            this.Price = 0;
+            this.Image = null;
+            this.Description = null;
+        }
+        public Phone(string id,string name,float price,string image,string description) {
+            this.ID = id;
+            this.Name = name;
+            this.Price = price;
+            this.Image = image;
+            this.Description = description;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
